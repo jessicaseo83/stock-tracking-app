70520b39b44589fc90abf26a04ea1fe2a5ae7529
Jessica Seo <jessicaseo83@gmail.com>
2021-03-13 16:58:07 -0500

    Add TopNav

--- stock-tracking/src/App.js
+++ stock-tracking/src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react';
+import TopNav from './components/TopNav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>stock trading app</h1>
+      <TopNav />
+      <h1>stock trading app</h1>
     </div>
   );
 }
