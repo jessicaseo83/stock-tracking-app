05103d8416353d09b1140dcaf7f75b201f53e90e
Jessica Seo <jessicaseo83@gmail.com>
2021-03-14 12:48:59 -0400

    Add login page

--- stock-tracking/src/App.js
+++ stock-tracking/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import {  BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import TopNav from './components/TopNav';
+import Login from './components/Login/Login';
 import './App.css';
 
 function App() {
   return (
+  <Router>
     <div className="App">
       <TopNav />
       <h1>stock trading app</h1>
     </div>
+    <Route path="/login">
+      <Login />
+    </Route>
+  </Router>
   );
 }
 
